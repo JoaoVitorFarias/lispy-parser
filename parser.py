3d3e559933e05e7ec4e13be13b252c74c1c42ca2
João Vitor <jvlopesfarias@gmail.com>
2020-12-12 21:44:47 -0300

    Finish analyzer

--- parser.py
+++ parser.py
@@ -9,7 +9,28 @@ class Symbol(NamedTuple):
 
 grammar = Lark(
     r"""
-start : "implemente-aqui!"
+    ?start : expr+ 
+
+    ?expr : atom | array | quoted
+    
+    quoted : "'" expr
+    
+    array : "(" expr+ ")"
+
+    ?atom : STRING -> string
+        |  NUMBER -> number
+        |  BOOLEAN -> boolean
+        |  CHAR -> char
+        |  NAME -> name
+        
+        
+        STRING : /"([^"\\\n\r\b\f]+|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*"/
+        NUMBER : /-?\d+(\.\d+)?([eE][+-]?\d+)?/ 
+        BOOLEAN : /\#t|\#f]/
+        CHAR : /\#\\[a-zA-Z]*/
+        NAME : /([a-zA-Z_%\+\-]|[-+=\/*!@$^&~<>?])[-?+*\/=<>\w]*/
+        %ignore /\s+/
+        %ignore /;;.*/ 
 """)
 
 
@@ -26,3 +47,30 @@ class LispyTransformer(InlineTransformer):
         "space": " ",
         "tab": "\t",
     }
+
+    def string(self, tk):
+        return eval(tk)
+
+    def number(self, tk):
+        return float(tk)
+
+    def boolean(self, tk):
+        return True if (tk == "#t") else False
+
+    def char(self, tk):
+        tk = tk.split('#\\')[-1]
+        return tk if tk.lower() not in self.CHARS else self.CHARS[tk.lower()]
+
+    def name(self, tk):
+        return Symbol(tk)
+
+    def array(self, *args):
+        return list(args)
+
+    def quoted(self, tk):
+        return [Symbol('quote'), tk]
+
+    def start(self, *args):
+        expr = list(args)
+        expr.insert(0, Symbol('begin'))
+        return expr
